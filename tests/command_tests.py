7389208d5f9be02028c6990cd4cedaa24b8dcdb0
Mike Lowen <mike@mlowen.com>
2013-05-13 22:40:32 +1200

    Added test for the Command class.
    Added the single_positional_test command test.

--- tests/command_tests.py
+++ tests/command_tests.py
@@ -2,12 +2,27 @@ from nose.tools import *
 from glint.command import *
 
 # test single positional
+def single_positional_handler(test):
+	assert_equal('test', test)
+
+def single_positional_test():
+	command = Command(single_positional_handler, '', '--')
+	
+	assert_equal(1, len(command.positional))
+	assert_equal(0, len(command.optional))
+	assert_equal(0, len(command.flags))
+	
+	command.run(['test'])
 
 # test two positional
+
+# test no positional supplied
 
 # test unexpected positional
 
 # test optional
+
+# test optional not supplied
 
 # test optional and positional
 
@@ -16,6 +31,8 @@ from glint.command import *
 # test optional with no value
 
 # test flag
+
+# test flag not supplied
 
 # test flag and positional
 
